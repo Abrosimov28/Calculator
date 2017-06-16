f113b73725a3eda83ef74a3730a5cc794606528b
Abrosimov28 <sashoMT@gmail.com>
2017-06-16 11:27:51 +0200

    Initial commit

---
+++ resources/js/main.js
@@ -0,0 +1,18 @@
+// = function
+function getResult() {
+  document.getElementById('calculation').innerHTML = document.getElementById('number').value;
+  document.getElementById('number').value = eval(document.getElementById('number').value);
+}
+
+// Will show the result upon hitting the enter key
+function enterGetResult(e){
+ var key=e.keyCode || e.which;
+  if (key==13){
+     getResult();
+  }
+}
+
+// CE function
+function removeLastChar() {
+  document.getElementById('number').value = document.getElementById('number').value.slice(0, -1);
+}
